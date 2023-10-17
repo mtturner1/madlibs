c8c64e521e76dcf503414926108ad587b1aa553e
Marcus Turner (Class of 2024) <242040mt@internal.salesianum.org>
2023-10-17 12:41:26 -0400

    first commit

---
+++ madlibs.py
@@ -0,0 +1,82 @@
+passage ='''The Eighty-Six werent buried in graves. 
+There could be no graves when there were no casualties. Even collecting their remains was
+forbidden. Pigs masquerading as humans were denied the right to rest in
+peace, or even the right to mourn their dead comrades.
+Therefore, if you were to define someone speaking a different tongue,
+someone of a different color, someone of a different heritage as a pig
+in human form, any oppression, persecution, or atrocity you might inflict
+upon them would never be regarded as cruel or inhumane.
+You should know to treat livestock as livestock. You simply cannot get
+those barbarian Eighty-Six to understand human ideals and nobility.'''
+
+start = 0
+finish = 15
+newPara = ""
+newPara += passage [start:finish]
+newText1 = input("Enter a verb:")
+newPara+= newText1 
+#print(newPara)
+start = 21 
+finish = 32 
+newPara += passage [start:finish]
+newText2 = input ("Enter a Noun:")
+newPara+= newText2
+#print(newPara)
+
+start = 39 
+finish = 48 
+newPara += passage [start:finish]
+newText3 = input ("Enter a Adverb:")
+newPara+= newText3
+#print(newPara)
+
+start = 52 
+finish = 128    
+newPara+= passage [start:finish] 
+newText4 = input ("Enter a Verb:")
+newPara+= newText4
+#print(newPara)
+
+start = 131
+finish = 144
+newPara += passage [start:finish]
+newText5 = input ("Enter a Noun:")
+newPara+= newText5
+#print(newPara) 
+
+start = 148
+finish = 202
+newPara += passage [start:finish]
+newText6 = input ("Enter a Preposition:")
+newPara+= newText6
+#print(newPara)
+
+start = 205
+finish = 234
+newPara += passage [start:finish]
+newText7 = input ("Enter a Noun:")
+newPara+= newText7
+#print(newPara)
+
+start = 239
+finish = 246
+newPara += passage [start:finish]
+newText8 = input ("Enter a Ajective:")
+newPara+= newText8 
+#print(newPara)
+
+start = 250
+finish = 262
+newPara += passage [start:finish]
+newText9 = input ("Enter an adverb:")
+newPara+= newText9
+print(newPara)
+#Print)newPara
+
+start = 271
+finish = 324
+newPara += passage [start:finish]
+newText10 = input ("Enter an Noun:")
+newPara+= newText10
+print(newPara)
+#Print(newPara)
